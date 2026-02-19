05398e2fa853b4573b28334e1bbff16360f265f3
sagivo <sagiv4@gmail.com>
2026-02-19 17:59:19 -0600

    lambda ready

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import type { Probot } from "probot";
+import { handlePullRequest } from "./handlers/pull-request.js";
+import { handleIssueComment } from "./handlers/comment.js";
+import { handleReviewComment } from "./handlers/review-comment.js";
+
+export function app(probot: Probot): void {
+  probot.log.info("PR Reviewer bot loaded");
+
+  probot.on(
+    ["pull_request.opened", "pull_request.synchronize", "pull_request.reopened"],
+    handlePullRequest,
+  );
+
+  probot.on("issue_comment.created", handleIssueComment);
+
+  probot.on("pull_request_review_comment.created", handleReviewComment);
+}
